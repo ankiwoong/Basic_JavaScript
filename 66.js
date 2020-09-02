96456efa6b8f1054525c899d8ee3dc95d30e360c
Ankiwoong <ankiwoong@gmail.com>
2020-09-02 09:58:40 +0900

    Comparisons with the Logical And Operator

---
+++ 66.js
@@ -0,0 +1,8 @@
+function testLogicalAnd(val) {
+    // Only change code below this line
+
+    if (val >= 25 && val <= 50) {
+        return "Yes";
+    } return "No";
+}
+console.log(testLogicalAnd(10));
