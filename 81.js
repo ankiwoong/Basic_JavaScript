0510d374176e46da092e7e0bd108cc6c0815910d
AnKiWoong <ankiwoong@gmail.com>
2020-10-12 13:55:44 +0900

    Build JavaScript Objects

---
+++ 81.js
@@ -0,0 +1,15 @@
+var myDog = {
+  // Only change code below this line
+  name: "Camper",
+  legs: 4,
+  tails: 1,
+  enemies: ["everything!"],
+  // Only change code below this line
+};
+
+var myDog = {
+  name: "Joe",
+  legs: 4,
+  tails: 1,
+  friends: ["many", "Ade"],
+};
